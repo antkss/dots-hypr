83a48ae3f7aa37d8908e0a0f2869e7fc3a8f22d1
antkss <anfixgapps@gmail.com>
2024-03-12 14:56:26 +0000

    update

--- config/ags/modules/overview/main.js
+++ config/ags/modules/overview/main.js
@@ -1,11 +1,10 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import { SearchAndWindows } from "./windowcontent.js";
-
-export default () => Widget.Window({
+import PopupWindow from "../.widgethacks/popupwindow.js";
+export default () => PopupWindow({
     name: 'overview',
     exclusivity: 'ignore',
     keymode: 'exclusive',
-    popup: true,
     visible: false,
     anchor: ['top'],
     layer: 'overlay',
