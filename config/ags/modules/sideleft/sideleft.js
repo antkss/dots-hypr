28d42f53ec3f011fda0f53e0232061c65469c334
antkss <anfixgapps@gmail.com>
2024-03-22 20:49:59 +0700

    update

--- config/ags/modules/sideleft/sideleft.js
+++ config/ags/modules/sideleft/sideleft.js
@@ -72,7 +72,7 @@ export default () => Box({
     // vertical: true,
     vexpand: true,
     hexpand: true,
-    css: 'min-width: 2px;',
+    css: 'min-width: 3px;',
     children: [
         EventBox({
             onPrimaryClick: () => App.closeWindow('sideleft'),
