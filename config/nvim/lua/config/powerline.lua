499376dc33c9fb8beea8ab67f3c8338995143773
antkss <anfixgapps@gmail.com>
2024-04-11 10:03:19 +0000

    update

--- config/nvim/lua/config/powerline.lua
+++ config/nvim/lua/config/powerline.lua
@@ -1,13 +1,13 @@
 local Colors = {
-  ibg		= '#FFB1C2',
-  bbg		= '#1D1013',
-  nbg		= '#FFB1C2',
-  vfg		= '#FFD9E0',
-  ifg		= '#BB0056',
-  rbg		= '#FFB599',
-  vbg		= '#693D3A',
-  cmdbg         = '#FFB1C2',
-  cmdfg         = '#8F0040',
+  ibg		= '#DDB8FF',
+  bbg		= '#16111B',
+  nbg		= '#DDB8FF',
+  vfg		= '#F0DBFF',
+  ifg		= '#8900EA',
+  rbg		= '#EDB5E1',
+  vbg		= '#563E5C',
+  cmdbg         = '#DDB8FF',
+  cmdfg         = '#6800B4',
 }
 local M = {
   normal = {
