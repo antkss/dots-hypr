5fb4524a8f5ceb3d76ee44183059b8263f7a82a3
antkss <anfixgapps@gmail.com>
2024-09-19 21:45:38 +0700

    update

--- source.sh
+++ source.sh
@@ -4,6 +4,7 @@ pacman-key --populate
 pacman -Sy
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 cp ./pacman.conf /etc
 
